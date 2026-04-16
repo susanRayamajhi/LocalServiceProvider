af46aad2f6047c6e43e66da4333a18ff5f5b2b28
vidhirami4-svg <vidhirami4@gmail.com>
2026-04-16 22:11:34 +0100

    Update service.controller.js

--- service.controller.js
+++ service.controller.js
@@ -1,38 +1,53 @@
 const Service = require('../models/service.model.js');
 
-// Get all services
+
 exports.getAll = (req, res) => {
     Service.getAll((err, data) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving services."
+        if (err) {
+            return res.status(500).send({
+                message: err.message || "Error retrieving services."
             });
-        else res.send(data);
+        }
+        res.send(data);
     });
 };
 
 // Search for services
 exports.search = (req, res) => {
-    const keyword = req.query.keyword;
+    const { keyword } = req.query;
+
+    if (!keyword) {
+        return res.status(400).send({
+            message: "Keyword is required for search."
+        });
+    }
+
     Service.search(keyword, (err, data) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while searching for services."
+        if (err) {
+            return res.status(500).send({
+                message: err.message || "Error searching services."
             });
-        else res.send(data);
+        }
+        res.send(data);
     });
 };
 
-// Get all partners for a service
+
 exports.getPartners = (req, res) => {
-    Service.getPartners(req.params.id, (err, data) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving partners."
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).send({
+            message: "Service ID is required."
+        });
+    }
+
+    Service.getPartners(id, (err, data) => {
+        if (err) {
+            return res.status(500).send({
+                message: err.message || "Error retrieving partners."
             });
-        else res.send(data);
+        }
+        res.send(data);
     });
 };
